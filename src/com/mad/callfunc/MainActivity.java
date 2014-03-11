e48e3720f02986d9fcf090527fa35919a0d08ac9
bipinvaylu@gmail.com <bipinvaylu@gmail.com>
2014-03-11 15:17:50 +0530

    Added phone number edittext

--- src/com/mad/callfunc/MainActivity.java
+++ src/com/mad/callfunc/MainActivity.java
@@ -7,21 +7,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class MainActivity extends Activity
 {
+
+	protected EditText mPhoneNo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		mPhoneNo = (EditText) findViewById(R.id.phoneNo);
 		findViewById(R.id.dial).setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
+				String strPhoneNo = mPhoneNo.getText().toString();
 				Intent callIntent = new Intent(Intent.ACTION_DIAL);
-				callIntent.setData(Uri.parse("tel:7383425349"));
+				callIntent.setData(Uri.parse("tel:" + strPhoneNo));
 				startActivity(callIntent);
 			}
 		});
@@ -30,9 +35,10 @@ public class MainActivity extends Activity
 
 			@Override
 			public void onClick(View v) {
+				String strPhoneNo = mPhoneNo.getText().toString();
 				// It's require permission android.permission.CALL_PHONE
 				Intent callIntent = new Intent(Intent.ACTION_CALL);
-				callIntent.setData(Uri.parse("tel:7383425349"));
+				callIntent.setData(Uri.parse("tel:" + strPhoneNo));
 				startActivity(callIntent);
 			}
 		});
